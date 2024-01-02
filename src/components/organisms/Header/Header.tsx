068545a334659e64ac8790129b623d6f97be8f5f
kazizi55 <kaz.redhot1201@gmail.com>
2024-01-02 17:33:17 +0900

    feat: 未実装のbuttonをコメントアウト

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { ProfileButton } from "./ProfileButton";
+// import { ThemeSwitchButton } from "./ThemeSwitchButton";
+// import { DocSearchButton } from "./DocSearchButton";
+
 export const Header: React.FC = () => {
   return (
-    <header>
-      <h1>blog.kazizi.dev</h1>
+    <header className="flex items-center justify-between py-8">
+      <Link href={"/"} className="text-2xl font-bold sm:block">
+        blog.kazizi.dev
+      </Link>
+
+      {/* TODO: kazizi.devの構築 */}
+      {/* <ProfileButton /> */}
+
+      {/* TODO: ロジックの実装 */}
+      {/* <ThemeSwitchButton /> */}
+      {/* <DocSearchButton /> */}
     </header>
   );
 };

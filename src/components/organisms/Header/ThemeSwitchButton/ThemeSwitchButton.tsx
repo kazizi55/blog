216da0458806f1933012acdc3b74a96c442362db
kazizi55 <kaz.redhot1201@gmail.com>
2024-01-02 16:27:28 +0900

    feat: add ThemeSwitchButton

---
+++ src/components/organisms/Header/ThemeSwitchButton/ThemeSwitchButton.tsx
@@ -0,0 +1,7 @@
+import { FaSun } from "@/components/atoms/Icon";
+// TODO: 切り替えロジックの実装
+// import { FaMoon } from "@/components/atoms/Icon";
+
+export const ThemeSwitchButton: React.FC = () => {
+  return <FaSun />;
+};

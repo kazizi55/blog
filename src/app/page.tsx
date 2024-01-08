622807a645f50a1b15ca729e3b5979645551db46
kazizi55 <kaz.redhot1201@gmail.com>
2024-01-09 00:04:41 +0900

    feat: display article list page dynamically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { ArticleList } from "@/components/templates/ArticleList";
+import { getFrontMatters } from "@/lib/grayMatter";
 
-export default function Home() {
+export default async function ArticleListPage() {
+  const frontMatters = await getFrontMatters();
+
   // TODO: i11n対応する
-  return <ArticleList />;
+  return <ArticleList articleListItems={frontMatters} />;
 }
